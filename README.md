a1c0bd73ae0232d9ca274fa3b8f052433fba95a9
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-10-11 17:53:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+To use this repository you need two tools: littlechef and librarian-chef
+
+### Setup your environment
+
+Install littlechef
+
+    pip install littlechef
+    
+Install librarian-chef
+
+    rvm --rvmrc --create use 1.9.3@littlechef-servers
+    gem install librarian
+    
+Run initial librarian install
+
+    librarian-chef install
+
+Setup your .ssh/config file. It needs to know the correct identify file (pem) to use with the servers. 
+Mine looks like this
+
+	Host *.compute-1.amazonaws.com
+	  User ubuntu
+	  IdentityFile /Users/scytacki/Development/ec2/genigames.pem
+
+### Setup a new lab.server
+
+1.  Spin up an ubuntu lucid server (could be AWS or could be elsewhere)
+   
+    If you are using AWS then use `ami-8e40e7e7` so step 2 can be skipped 
+
+2.  Install ChefSolo
+
+        fix node:$HOSTNAME_OF_SERVER deploy_chef
+
+3.  Install lab.server
+        
+        fix node:$HOSTNAME_OF_SERVER role:lab-server
+
+### Adding new features to the lab.server configuration
+
+1.  Spin up your own instance using the steps above. 
+
+2.  Modify the necessary files to add the feature
+    - `roles/lab-server.json` for changing an attribute or adding a recipe
+    - `site-cookbooks/lab-server` for changing default deploy steps
+    - `site-cookbooks/node-couch-webapp` for changing node and couch configuration
+    - `Chefile` to use a new cookbook that isn't listed there see the "Add New Cookbook" section
+
+3.  Update your instance
+
+        fix node:$HOSTNAME_OF_SERVER
+
+4.  Make sure it builds from scratch, redoing the "Setup a new lab.server" steps
+
+5.  To put these features on the public server you have 2 options 
+    -   copy the necessary data from the old server to the new one, and switch the ElasticIP to the new server
+    -   update the public version (Note: it isn't part of this system yet so this command won't work yet)
+
+            fix node:lab.dev.concord.org
+
+### Adding a New Cookbook
+
+There are 2 types of cookbooks supported: cookbooks and site-cookbooks.
+
+*Cookbooks* are managed by librarian-chef. These are pulled from remote source. The source can be a github
+repository, or a chef repository
+
+You add a new one by modifying the Chefile so librarian-chef knows about the new cookbook. Then 
+you run
+
+    librarian-chef install
+
+*Site-cookbooks* are local to this repository. In littlechef terms this "repository" is called a kitchen. 
+Add a folder to the site-cookbooks folder. The folder needs the standard Chef cookbook format.
+
+
+### TODO
+
+Improve the Indentify file managment. If we are going to use a single kitchen for all of our servers then we'll probably
+want to have several pem files, and then all the developers would need to manage the mapping of all of those files.
+
+Fix the way nodejs is installing the apt repository so we don't need to run apt-get update ourselves
+
+Separate out the couchdb from package installation code into its own recipe, look around for other couch
+cookbooks to see if anyone wants this as an option.
---
+++ site-cookbooks/lab-server/recipes/default.rb
@@ -0,0 +1,212 @@
+# perhaps these should just be put in the runlist instead??
+
+package "python-software-properties"
+package "tree"
+
+include_recipe "java"
+
+# https://launchpad.net/ubuntu/precise/amd64/openjdk-6-jdk
+package "openjdk-6-jdk" do
+  action :install
+  version "6b27-1.12.6-1ubuntu0.12.04.2"
+end
+
+package "libicu-dev" do
+  action :install
+end
+
+# we need this concord maven provider to resolve
+# some legacy artifacts used by otrunk
+
+directory "/home/deploy/.m2" do
+  owner "deploy"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+cookbook_file "/home/deploy/.m2/settings.xml" do
+  source "settings.xml"
+  owner "deploy"
+  group "root"
+  mode "664"
+end
+
+# add the node repository here so we can make sure that apt-get update runs correctly
+# https://launchpad.net/~chris-lea/+archive/node.js/
+apt_repository 'node.js' do
+  uri 'http://ppa.launchpad.net/chris-lea/node.js/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver "keyserver.ubuntu.com"
+  key "C7917B12"
+  action :add
+end
+
+# ppa for the couchdb repository
+# https://launchpad.net/~longsleep/+archive/couchdb
+# apt_repository 'couchdb' do
+#   uri 'http://ppa.launchpad.net/longsleep/couchdb/ubuntu'
+#   distribution node['lsb']['codename']
+#   components ['main']
+#   keyserver "keyserver.ubuntu.com"
+#   key "56A3D45E "
+#   action :add
+# end
+
+# the apt_repository tries to do this itself, but it isn't using
+# the root user so it seems to fail
+execute "apt-get update" do
+  action :run
+  user "root"
+end
+
+include_recipe "apache2"
+
+package "apache2" do
+  action :install
+  # version "2.2.22-1ubuntu1.3"
+end
+
+# include_recipe "nodejs"
+# installing nodejs now also installs npm
+package "nodejs" do
+  action :install
+  action :upgrade
+end
+
+# package "couchdb" do
+#   action :install
+#   version "1.2.1-0ppa1+0"
+# end
+
+# the couchdb service script creates this folder but does so as root
+# instead of the couchdb user
+# directory "/var/run/couchdb" do
+#   owner "couchdb"
+#   group "root"
+#   mode "755"
+# end
+
+directory "/var/www" do
+  owner "deploy"
+  group "root"
+  mode "755"
+end
+
+directory "/var/www/log" do
+  owner "www-data"
+  group "www-data"
+  mode "755"
+end
+
+script "update_npm" do
+  interpreter "bash"
+  user "deploy"
+  code "sudo npm update -g npm"
+end
+
+script "setup_coffee" do
+  interpreter "bash"
+  user "deploy"
+  code "sudo npm install -g coffee-script"
+end
+
+# service "couchdb" do
+#   action :start
+# end
+
+group "rvm" do
+  append true
+  members ["deploy", "ubuntu"]
+end
+
+cookbook_file "/home/deploy/.rvmrc" do
+  source "rvmrc"
+  owner "deploy"
+  group "deploy"
+  mode "664"
+end
+
+cookbook_file "/home/deploy/.bash_login" do
+  source "dot_bash_login"
+  owner "deploy"
+  group "deploy"
+  mode "664"
+end
+
+execute "enable the locate database" do
+  command "sudo updatedb"
+end
+
+execute "enable access to a default locale" do
+  command "sudo locale-gen en_US"
+end
+
+# Clone Lab framework with embedded web application
+git "/var/www/app" do
+  user "deploy"
+  group "root"
+  repository "git://github.com/labframework/lab.git"
+  action :sync
+end
+
+# Create tmp dir in web app so Passenger restarts work by touching tmp/restart.txt
+directory "/var/www/app/tmp" do
+  owner "deploy"
+  group "root"
+  mode "775"
+end
+
+execute "fix-permissions" do
+  user "deploy"
+  command <<-COMMAND
+  sudo chown -R deploy:root /var/www/app/*
+  sudo chmod -R ug+rw /var/www/app/*
+  sudo chgrp -R deploy /usr/local/rvm/*
+  sudo chmod -R g+w /usr/local/rvm/*
+  COMMAND
+end
+
+# create global git configuration for deploy user
+template "/home/deploy/.gitconfig" do
+  source "dot-gitconfig.erb"
+  mode 0664
+  owner "deploy"
+  group "deploy"
+end
+
+if ::File.exist?("/var/www/app/config/config.yml")
+  puts "config/config.yml exists"
+else
+  ruby_block "Creating default config/config.yml" do
+   block do
+    ::FileUtils.cp "/var/www/app/config/config.sample.yml", "/var/www/app/config/config.yml"
+    ::FileUtils.chown 'deploy', 'root', "/var/www/app/config/config.yml"
+   end
+  end
+end
+
+link "/usr/local/bin/mvn" do
+  to "/usr/local/maven-3.1.0/bin/mvn"
+end
+
+# # maven needs this to work if JAVA_HOME isn't defined
+# link "/usr/lib/jvm/default-java" do
+#   to "/usr/lib/jvm/java-6-openjdk-amd64/jre"
+# end
+#
+
+# execute "disable-default-apache2-site" do
+#   command "sudo a2dissite default"
+#   notifies :reload, resources(:service => "apache2"), :delayed
+# end
+
+web_app "lab" do
+  cookbook "apache2"
+  server_name node['lab-hostname']
+  server_aliases [node['lab-hostname']]
+  docroot "/var/www/app/public"
+  enable true
+  notifies :restart, resources(:service => "apache2"), :delayed
+end
